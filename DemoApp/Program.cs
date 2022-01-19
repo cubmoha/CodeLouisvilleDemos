92de238f9f089859b9f92d4478558e14f57e3fa2
Mohammed <mohammedmills41@gmail.com>
2022-01-19 03:06:16 -0500

    first commit

---
+++ DemoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int num = 3;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
